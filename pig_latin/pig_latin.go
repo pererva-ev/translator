39d145542cbbc3361857cccdf571d64338e718d4
Evheniya <evheniya.pererva@corezoid.com>
2020-12-14 12:53:01 +0200

    add pig latin

---
+++ pig_latin/pig_latin.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	pigLatinSuffix             string = "ay"
+	firstLetterExceptions      string = "aeiou"
+	firstLetterExceptionSuffix string = "d" + pigLatinSuffix
+)
+
+// Translate translates one or more english words into the PigLatin equlivent
+func Translate(in string) string {
+	var pigLatinWords []string
+	englishWords := strings.Split(in, " ")
+
+	for _, word := range englishWords {
+		first := word[0:1]
+		if strings.Contains(firstLetterExceptions, first) {
+			pigLatinWords = append(pigLatinWords, word+firstLetterExceptionSuffix)
+		} else {
+			pigLatinWords = append(pigLatinWords, word[1:]+first+pigLatinSuffix)
+		}
+	}
+	return strings.Join(pigLatinWords, " ")
+}
+
+func main() {
+	word := string(Translate("hello"))
+	fmt.Printf(word)
+}
